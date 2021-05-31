2963b99e7494d5aded9279422eca8eae0a486b36
GIULIT <85126637+GIULIT@users.noreply.github.com>
2021-05-31 11:54:30 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,90 @@
+def on_left_pressed():
+    heroina.set_image(img("""
+        . . . . f f f f f f . . . . . . 
+                . . . f 2 f e e e e f f . . . . 
+                . . f 2 2 2 f e e e e f f . . . 
+                . . f e e e e f f e e e f . . . 
+                . f e 2 2 2 2 e e f f f f . . . 
+                . f 2 e f f f f 2 2 2 e f . . . 
+                . f f f e e e f f f f f f f . . 
+                . f e e 4 4 f b e 4 4 e f f . . 
+                . . f e d d f 1 4 d 4 e e f . . 
+                . . . f d d d d 4 e e e f . . . 
+                . . . f e 4 4 4 e e f f . . . . 
+                . . . f 2 2 2 e d d 4 . . . . . 
+                . . . f 2 2 2 e d d e . . . . . 
+                . . . f 5 5 4 f e e f . . . . . 
+                . . . . f f f f f f . . . . . . 
+                . . . . . . f f f . . . . . . .
+    """))
+controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
+
+def on_overlap_tile(sprite, location):
+    game.over(True, effects.blizzard)
+scene.on_overlap_tile(SpriteKind.player,
+    sprites.dungeon.collectible_blue_crystal,
+    on_overlap_tile)
+
+def on_right_pressed():
+    heroina.set_image(img("""
+        . . . . . . . . . . . . . . . . 
+                . . . . . f f f f f f . . . . . 
+                . . . f f e e e e f 2 f . . . . 
+                . . f f e e e e f 2 2 2 f . . . 
+                . . f e e e f f e e e e f . . . 
+                . . f f f f e e 2 2 2 2 e f . . 
+                . . f e 2 2 2 f f f f e 2 f . . 
+                . f f f f f f f e e e f f f . . 
+                . f f e 4 4 e b f 4 4 e e f . . 
+                . f e e 4 d 4 1 f d d e f . . . 
+                . . f e e e e e d d d f . . . . 
+                . . . . f 4 d d e 4 e f . . . . 
+                . . . . f e d d e 2 2 f . . . . 
+                . . . f f f e e f 5 5 f f . . . 
+                . . . f f f f f f f f f f . . . 
+                . . . . f f . . . f f f . . . .
+    """))
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def on_a_repeated():
+    if heroina.is_hitting_tile(CollisionDirection.BOTTOM):
+        heroina.vy = -150
+controller.A.on_event(ControllerButtonEvent.REPEATED, on_a_repeated)
+
+heroina: Sprite = None
+scene.set_background_color(9)
+heroina = sprites.create(img("""
+        . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . .
+    """),
+    SpriteKind.player)
+controller.move_sprite(heroina, 100, 100)
+tiles.set_tilemap(tilemap("""
+    level1
+"""))
+scene.camera_follow_sprite(heroina)
+heroina.set_position(10, 0)
+heroina.ay = 300
+info.set_life(3)
+
+def on_on_update():
+    if heroina.tile_kind_at(TileDirection.BOTTOM, assets.tile("""
+        tile1
+    """)):
+        heroina.set_position(10, 0)
+        info.change_life_by(-1)
+game.on_update(on_on_update)
---
+++ tilemap.g.ts
@@ -0,0 +1,48 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`200010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000301000001000001000101000101000001000000000100010101000100000001010202020202020202020202020202020202020202020202020202020202020202`, img`
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+................................
+2..2..2.22.22..2....2.222.2...22
+2..2..2.22.22..2....2.222.2...22
+`, [myTiles.transparency16,sprites.builtin.brick,myTiles.tile1,sprites.dungeon.collectibleBlueCrystal], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "tile1":return tile1;
+            case "tile2":return tile2;
+            case "tile3":return tile3;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
